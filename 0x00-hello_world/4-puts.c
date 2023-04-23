32262d7109278b289db769c33446f3f78ea24ed5
Joy <atienojoey@gmail.com>
2023-04-23 02:49:25 -0700

    using puts to print in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ * main - using puts to print in C
+ * Return - 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
