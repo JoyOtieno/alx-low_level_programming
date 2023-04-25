9886d7b50f209454bc625175f98e21c60b22c277
Joy <atienojoey@gmail.com>
2023-04-25 12:06:42 -0700

    putchar letters on ASCII table

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * Description: 'putchar to print lower and upper case letters using ASCII decimal values'
  * Return 0 (Success)
  */
 int main(void)
