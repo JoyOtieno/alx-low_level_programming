c5dbc335da831ff7eb118c6ec476f65ee2fdda44
Joy <atienojoey@gmail.com>
2023-04-25 12:58:51 -0700

    base ten numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Printing all numbers of base ten.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	while (m <= 10)
+	{
+		printf("%d\n", m);
+		m++;
+	}
+	return (0);
+}
