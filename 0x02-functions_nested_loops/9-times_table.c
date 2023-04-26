3a0f8b5e342cd200581811da26d50a0b9b3fb8f9
Joy <atienojoey@gmail.com>
2023-04-26 15:08:53 -0700

    printing x9 time tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,6 @@ void times_table(void)
 	int num, multiplier, product;
 
 	for (num = 0; num < 10; num++)
-
 	{
 		for (multiplier = 0; multiplier < 10; multiplier++)
 		{
@@ -16,25 +15,21 @@ void times_table(void)
 			{
 				_putchar(product + '0');
 			}
-			else if (product < 10)
-			{
-				_putchar(product + '0');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-			if (multiplier == 9)
-			{
-				_putchar('\n');
-			}
-			else
+			if (product < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
